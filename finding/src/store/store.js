967b90358f412563984135187570f42fa0055562
zeeshanshanif <zee81zee@yahoo.com>
2023-02-14 18:10:50 +0500

    created redux store

---
+++ finding/src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
+import 
+
+
+export default function (){
+    return configureStore({})
+}
