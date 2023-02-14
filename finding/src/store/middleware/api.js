27a6225e32c3ec19bb57bdd9d79082ea218090d4
zeeshanshanif <zee81zee@yahoo.com>
2023-02-14 18:11:09 +0500

    added api for finding falcons

---
+++ finding/src/store/middleware/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { baseURL } from '../constant';
+
+const api = ({ dispatch}) => next => async action =>{
+    if (action.type !== "apiCallBegin") return next(action);
+    next(action);
+    const { url, method, data, onSuccess, onError} = action.payload;
+    try{
+        const response = await axios.request({
+            baseURL, url, method, data
+
+        })
+        dispatch({ type:onSuccess, payload: response.data});
+    } catch (err) {
+        dispatch({ type: "onError", error:`${error.message} (${onError})`});
+    }
+}
+export default api;
