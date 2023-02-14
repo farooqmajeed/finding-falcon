2d052b561acb1ec043970ba956e79b4a42c91283
zeeshanshanif <zee81zee@yahoo.com>
2023-02-14 18:10:33 +0500

    added actions

---
+++ finding/src/store/action/actions.js
@@ -0,0 +1,16 @@
+import { createAction } from "@reduxjs/toolkit";
+
+
+export const fetchPlanetAction = createAction("fetchPlanetAction");
+export const fetchVehiclesAction = createAction("fetchVehiclesAction");
+
+export const getInitialDestinationAction = createAction("getInitialDestinationAction");
+export const addDestinationAction = createAction("addDestination");
+export const vehicleSelectedAction = createAction("vehicleSelected");
+
+export const findFalconAction = createAction("findFalconAction");
+export const fetchTokenAction = createAction("fetchTokenAction");
+
+export const onError = createAction("onError");
+export const resetErrorAction = createAction("resetErrorAction");
+
